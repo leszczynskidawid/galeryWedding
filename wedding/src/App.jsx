ec9f3d0e50604dc6c6bac405c2176c4ff1a173fd
Dawid Leszczynski <dawid.leszczynski1297@gmail.comgit config --global user.email dawid.leszczynski1297@gmail.comgit config --global user.email dawid.leszczynski1297@gmail.com>
2025-02-25 13:32:28 +0100

    poprawa

--- wedding/src/App.jsx
+++ wedding/src/App.jsx
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./Header/Header";
 import {
