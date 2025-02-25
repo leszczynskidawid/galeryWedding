5f111dc6de7fdeb1fd617fb22ae4b68c12b87ffc
Dawid Leszczynski <dawid.leszczynski1297@gmail.comgit config --global user.email dawid.leszczynski1297@gmail.comgit config --global user.email dawid.leszczynski1297@gmail.com>
2025-02-25 13:33:25 +0100

    deploy

--- wedding/src/App.jsx
+++ wedding/src/App.jsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react";
-import reactLogo from "./assets/react.svg";
+// import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./Header/Header";
